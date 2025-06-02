d2202ab9ec12ebf5f2d4f36d1782aaed441d9813
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-06-02 12:26:35 +0200

    minimal mvc hello world

--- crudApp/Controllers/HomeController.cs
+++ crudApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Mvc;
 using crudApp.Models;
 
@@ -6,26 +7,10 @@ namespace crudApp.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
+    // get hello world
     public IActionResult Index()
     {
-        return View();
+        return Content("Hello World!");
     }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    
 }
